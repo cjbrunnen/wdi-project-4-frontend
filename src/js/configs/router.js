ab2edb2cf9ba404c09e6b6ecf39350150c49cb08
cjbrunnen <cazgotsoul@gmail.com>
2016-10-25 11:37:22 +0100

    log out functionality started

--- src/js/configs/router.js
+++ src/js/configs/router.js
@@ -25,7 +25,37 @@ function Router($stateProvider, $locationProvider, $urlRouterProvider){
     url: "/users",
     templateUrl:  "/js/views/users/index.html",
     controller:   "usersIndexCtrl as usersIndex",
+  })
+  .state("mainIndex", {
+    url: "/index",
+    templateUrl: "/js/views/mainIndex.html",
+    controller:  "mainIndexCtrl as mainIndex"
   });
+// .state("usersNew", {
+//     url: "/users/new",
+//     templateUrl: "/js/views/users/new.html",
+//     controller: "UsersNewCtrl as usersNew"
+// }
+// .state("postsNew", {
+//     url: "/posts/new",
+//     templateUrl: "/js/views/posts/new.html",
+//     controller: "PostsNewCtrl as postsNew"
+// }
+// .state("usersEdit", {
+//     url: "/users/:id/edit",
+//     templateUrl: "/js/views/users/edit.html",
+//     controller: "UsersEditCtrl as usersEdit"
+//   })
+//   .state("postsEdit", {
+//       url: "/posts/:id/edit",
+//       templateUrl: "/js/views/posts/edit.html",
+//       controller: "PostsEditCtrl as postsEdit"
+//     })
+  // .state('usersShow', {
+  //   url: '/users/:id',
+  //   templateUrl: '/js/views/users/show.html',
+  //   controller: 'UsersShowCtrl as users'
+  // })
 
   $urlRouterProvider.otherwise("/");
 }
